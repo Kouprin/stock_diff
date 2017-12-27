80d650be88684453cc50bb3852b0873ff15a26ca
Alex <kouprinal@yandex.ru>
2017-12-27 13:00:06 +0300

    poloniex + kraken BTC to USD + test

---
+++ test.py
@@ -0,0 +1,11 @@
+import api.poloniex as poloniex
+import api.kraken as kraken
+import pprint
+
+p = poloniex.poloniex(0, 0)
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint("poloniex " + p.api_query("returnTicker")["USDT_BTC"]['last'])
+
+k = kraken.API()
+open_positions = k.query_public('Ticker', {'pair':'XXBTZUSD'})
+pp.pprint("kraken " + open_positions['result']['XXBTZUSD']['c'][0])
